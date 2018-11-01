d9ab6701f44d5bfca8d419e423f6f2b9cdbf5358
Femi Coker <fcoker@outlook.com>
2018-11-01 10:13:50 +0000

    set up for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 //setting up port to be exported tp app.js to be listened to
 //!!-->ask what dbUri does<--!!
 const port = process.env.PORT || 4000;
-const dbUri = process.env.MONGOD_URI || 'mongodb://localhost/ciruit-review-site';
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/ciruit-review-site';
 
 module.exports = {
   dbUri: dbUri,
